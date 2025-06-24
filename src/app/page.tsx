e9b5eed31b89cc0a52a9a57a639ab198766c1227
Fabio Teixeira - EIN - TSI <fabio5413@ica.com>
2025-06-24 11:55:02 -0300

    menu update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,47 +6,53 @@ import { useRouter } from 'next/navigation';
 
 
 
-
-
 export default function Home() {
+
+
+  const router = useRouter(); // ✅ Instância do roteador do Next.js
+  const playSoundAndNavigate = useCallback((path: string) => {
+  const audio2 = new Audio("/sounds/button.mp3");
+  audio2.play();
+  setTimeout(() => {
+    router.push(path); // Redireciona após tocar o som
+  }, 150); // Pequeno delay para o som iniciar
+  }, [router]);
+
   return(
 
     <div className="flex justify-center items-center select-none overflow-hidden">
-      <div className="fixed z-[999]">
-        <img src="spring_menu.gif" alt="spring" className=" h-250 opacity-30" />
+      <div className='relative h-screen w-screen overflow-hidden bg-black '></div>
+
+         <div id="botões" className="fixed mt-80 mr-190 " style={{zIndex:10000}}>
+        <button onMouseEnter={() => playSoundAndNavigate('/')} className="text-[25px]"   style={{ textShadow: "1px 2px 10px #454a0d" }}>NEW GAME</button>
+        <br></br><br></br><button onMouseEnter={() => playSoundAndNavigate('/')} className="text-[25px]"style={{ textShadow: "1px 2px 10px #454a0d" }}>LOAD GAME</button>
+      </div>   
+
+
+      <div className="relative h-screen  overflow-hidden bg-red">
+
+
+      <div className="fixed ml-50 mt-20 inset-0 pointer-events-none glitch-layer  ">
+      <div className="fixed">
+        <img src="spring_menu.gif" alt="spring" className="ml-70 h-150 opacity-30" />
       </div>
-      <div className="relative h-screen w-screen overflow-hidden bg-black z-[1]">
-      {/* camada com glitch */}
-
-        <div className="fixed ml-50 mt-20 inset-0 pointer-events-none glitch-layer z-[1] ">
-          <h1 className="fixed  text-[40px] z-99">
-            <p  className="text-white  font-bold"
-            style={{ textShadow: "2px 2px 10px #454a0d" }}>F<br></br></p>
-            <p   className="text-white  font-bold"
-            style={{ textShadow: "1px 2px 10px #454a0d" }}>N<br></br></p>
-            <p  className="text-white  font-bold"
-            style={{ textShadow: "1px 2px 10px #454a0d" }}>A<br></br></p>
-            <p   className="text-white  font-bold"
-            style={{ textShadow: "1px 2px 10px #454a0d" }}>F<br></br></p>
-            <p   className="text-white  font-bold"
-            style={{ textShadow: "1px 2px 10px #454a0d" }}>3<br></br></p>
-          </h1>
-          
-          <div id="botões" className="flex flex flex-col justify-center items-center
-          mt-100 mr-230  Z-[999]">
-            <button className="text-[25px]"style={{ textShadow: "1px 2px 10px #454a0d" }}>NEW GAME</button>
-            <button className="text-[25px]"style={{ textShadow: "1px 2px 10px #454a0d" }}>LOAD GAME</button>
-          </div>
-
-      
 
 
 
+        <h1 className="fixed  text-[40px] ">
+          <p  className="text-white  font-bold"
+          style={{ textShadow: "2px 2px 10px #454a0d" }}>F<br></br></p>
+          <p   className="text-white  font-bold"
+          style={{ textShadow: "1px 2px 10px #454a0d" }}>N<br></br></p>
+          <p  className="text-white  font-bold"
+          style={{ textShadow: "1px 2px 10px #454a0d" }}>A<br></br></p>
+          <p   className="text-white  font-bold"
+          style={{ textShadow: "1px 2px 10px #454a0d" }}>F<br></br></p>
+          <p   className="text-white  font-bold"
+          style={{ textShadow: "1px 2px 10px #454a0d" }}>3<br></br></p>
+        </h1>
         </div>
-        {/* conteúdo do menu */}
-        <div className="relative z-10 flex justify-center items-center h-full z-[1]"></div>
-      </div>
-
+        </div>
     </div>
   )
 }
