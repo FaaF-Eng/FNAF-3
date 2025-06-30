270f0d290d1ce4d2b504ab796b7db945ee810f75
Fabio Teixeira - EIN - TSI <fabio5413@ica.com>
2025-06-30 10:49:16 -0300

    scroll

--- src/app/pagina4/page.tsx
+++ src/app/pagina4/page.tsx
@@ -1,35 +1,56 @@
 'use client'
 
-
-import { useState, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Page4() {
+  const scrollRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const container = scrollRef.current;
+      if (!container) return;
+
+      const { clientX } = e;
+      const screenWidth = window.innerWidth;
+      const ratio = clientX / screenWidth;
+
+      const maxScroll = container.scrollWidth - container.clientWidth;
+      container.scrollLeft = maxScroll * ratio;
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
 
   return (
-    <div className=" w-400 h-full select-none overflow-y-hidden overflow-x-auto">   
-    <div className='absolute mt-155 w-full '>
-      <button className='ml-30 
-      w-70 h-10 bg-[#8d5700] text-white rounded-[6px] text-[20px] p-0 m-0 leading-none
-      cursor-pointer transition-colors duration-300 hover:bg-orange-900 '
-      >⏷CLICK⏷</button>
-    </div>
+    <div
+      ref={scrollRef}
+      className="relative h-full w-full overflow-x-auto overflow-y-hidden select-none"
+    >
+      <div className="w-[2000px] h-full relative"> {/* conteúdo mais largo que a tela */}
 
-    <div className='absolute mt-55 w-full '>
-      <button className='ml-375 
-      w-20 h-70 bg-[#00388d] border-red-500 text-white rounded-[6px] text-[20px] p-0 m-0 leading-none
-      cursor-pointer transition-colors duration-300 hover:bg-blue-500 '
-      >⏵<br></br><br></br>CLICK<br></br><br></br>⏵</button>
-    </div>
-    
+        {/* Botão esquerdo */}
+        <div className='absolute top-155 left-30'>
+          <button className='w-70 h-10 bg-[#8d5700] text-white rounded-[6px] text-[20px] leading-none
+          cursor-pointer transition-colors duration-300 hover:bg-orange-900'>
+            ⏷CLICK⏷
+          </button>
+        </div>
 
-      <img
-        src="/sala-fnaf2.webp"
-        alt="Sala de segurança"
-        className=" z-[1] "
-      />
+        {/* Botão direito */}
+        <div className='absolute top-55 left-475'>
+          <button className='w-20 h-70 bg-[#00388d] text-white rounded-[6px] text-[20px] leading-none
+          cursor-pointer transition-colors duration-300 hover:bg-blue-500'>
+            ⏵<br /><br />CLICK<br /><br />⏵
+          </button>
+        </div>
+
+        <img
+          src="/sala-fnaf2.webp"
+          alt="Sala de segurança"
+          className="z-[1]"
+        />
+      </div>
     </div>
-    
   );
 }
-
